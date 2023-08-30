bf57cbee258a1833eaa41fdce04e13f4db4ec6be
aboublini <aboublini@gmail.com>
2023-08-30 15:22:34 +0300

    final

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
 
   useEffect(
     () => {
+      // Retrieve data
       const fetchData = async () => {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
         const data = await response.json();
         setData(data);
       };
@@ -19,20 +20,25 @@ function App() {
     []
   );
 
-  console.log(data);
+  // console.log(data);
   
 
-  const contents = {
-    title: "This is the card's title",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  } 
+  // const contents = {
+  //   title: "This is the card's title",
+  //   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  // } 
   return <div className="App">
     {/* <Card
       title={contents.title}
       text={contents.text}
     /> */}
 
-
+      {data.map(post => (
+        <Card
+          title={post.title}
+          text={post.body}
+        />
+      ))}
   </div>;
 }
 
